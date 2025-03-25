e951eda7b8dd78c65a690c4c1dfbf222195fc09c
Arthur PAILLE <a.paille@lyceesaintefamille.com>
2025-03-25 12:23:43 +0100

    Partie 1

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
+$servername = "localhost"; 
+$username = "root";  
+$password = "root";  
+$dbname = "junia.sql"; 
 
-echo "Hed!";
+try {
+    $dbPDO = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password); 
+    
+     $dbPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+     echo "Connexion réussie à la base de données"; 
 
+} catch(PDOException $e) {
+ 
+    echo "La connexion a échoué : " . $e->getMessage();
+}
 ?>
